b0eba9971a1d1d0456d44408f4114c7535ffc7ce
Javier <jrodriguez@blucapital.mx>
2023-10-13 23:18:51 -0600

    Add api key authorization method

--- src/config.py
+++ src/config.py
@@ -7,6 +7,7 @@ class Config:
 
 class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "postgresql://postgres:postgres@localhost/test_db")
+    API_KEY = os.environ.get("API_KEY", "")
 
 
 config_settings = {"development": DevelopmentConfig}
--- src/routes/routes.py
+++ src/routes/routes.py
@@ -1,28 +1,38 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, current_app
 from flask_cors import cross_origin
 from src import transformer, predictor
 from src.models import db
 from src.models.invoice_prediction_log import InvoicePredictionLog
 import os
-routes_blueprint = Blueprint('main', __name__,)
 
-@routes_blueprint.route('/', methods=["GET"])
+routes_blueprint = Blueprint(
+    "main",
+    __name__,
+)
+
+
+def is_valid(api_key):
+    return api_key == current_app.config["API_KEY"]
+
+@routes_blueprint.route("/", methods=["GET"])
 def home():
     return "Nothing to show"
 
 
 @routes_blueprint.route("/predict", methods=["POST"])
 @cross_origin()
-def predict_late_invoice(): 
+def predict_late_invoice():
+    if not is_valid(request.headers.get('API-KEY')):
+        return {"message": "You must provide a valid api key"}, 401
+
     params = request.get_json(force=True)
-    
     req = dict((k, params[k]) for k in ['factura_importe', 'fecha_fin', 'fecha_inicio', 'fecha_nacimiento', 'linea_limite', 'pagador_rfc', 'porcentaje_adelanto']
            if k in params)
-    
+
     data = transformer.transform(req)
 
     result = predictor.predict(data)
-        
+
     log = InvoicePredictionLog(params['folio'], params, result['prediction'][0])
     db.session.add(log)
     db.session.commit()
